6747cb36640485e38cea7b52591495ca6294b8c8
Isabelle Glasmacher <isabelle_glasmacher@hotmail.de>
2024-12-01 20:35:44 +0100

    solve simililarity score

--- 01/index.ts
+++ 01/index.ts
@@ -1,7 +1,9 @@
 import {readFile} from './readFile.ts'
 import {calculateTotalDistance} from './calculateTotalDistance.ts'
+import { calculateSimilarityScore } from './calculateSimilarityScore.ts';
 
 console.log('Solving advent of code day 1');
 
 const data = readFile('01/input.txt')
-console.log(calculateTotalDistance(data[0], data[1]))
+console.log('distance is '+ calculateTotalDistance(data[0], data[1]))
+console.log('similarity score is '+ calculateSimilarityScore(data[0], data[1]))
