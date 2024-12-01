ee8f535609b1352c56e2af035f2c710c85dc2cfd
Isabelle Glasmacher <isabelle_glasmacher@hotmail.de>
2024-12-01 19:55:51 +0100

    add calculation of distance

--- 01/index.ts
+++ 01/index.ts
@@ -1,5 +1,16 @@
-console.log('hello world');
+console.log('Solving advent of code day 1');
 
-export function calc(): number {
-    return 0;
+export function calc(locationList1: number[], locationList2: number[]): number {
+    if (locationList1.length !== locationList2.length) {
+        throw new Error('there should be equal amount of locations')
+    }
+    const sortedLocation1 = locationList1.sort()
+    const sortedLocation2 = locationList2.sort()
+
+    let sum = 0;
+    for (let i=0;i<locationList1.length;i++) {
+        sum += Math.abs(sortedLocation1[i]-sortedLocation2[i])
+    }
+
+    return sum;
 }
--- 01/test.ts
+++ 01/test.ts
@@ -2,7 +2,21 @@ import {describe, it, expect } from '@jest/globals'
 import { calc } from '.'
 
 describe('day 01', ()=>{
-    it('', ()=>{
-        expect(calc()).toBe(0)
+    it('should calculate correctly on sorted array', ()=>{
+        expect(calc(
+            [1, 2, 3], 
+            [2, 4, 5]
+        )).toBe(5)
+    })
+    
+    it('should calculate correctly on unsorted array', ()=>{
+        expect(calc(
+            [2, 3, 1], 
+            [5, 4, 2]
+        )).toBe(5)
+    })
+
+    it('should return fail with uneven array lengths', ()=>{
+        expect(()=>calc([], [1])).toThrow()
     })
 })
