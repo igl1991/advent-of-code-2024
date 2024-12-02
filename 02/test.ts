dc9a9b29bc15bd73c74f38db262d365122238b8b
Isabelle Glasmacher <isabelle_glasmacher@hotmail.de>
2024-12-02 17:20:33 +0100

    implement check if report is save

--- 02/test.ts
+++ 02/test.ts
@@ -1,8 +1,33 @@
 import { describe, expect, it } from "@jest/globals";
 import { reportIsSave } from ".";
+import exp from "constants";
 
 describe('day 02 - part 1', ()=>{
-    it('should mark report as save', ()=> {
+    it('should mark report as save with decrease', ()=> {
         expect(reportIsSave([7,6,4,2,1])).toBe(true)
+    })
+
+    it('should mark report as unsafe for wrong level increase', ()=> {
+        expect(reportIsSave([1,2,7,8,9])).toBe(false)
+    })
+    
+    it('should mark report as unsafe for wrong level decrease', ()=> {
+        expect(reportIsSave([9,7,6,2,1])).toBe(false)
+    })
+
+    it('should mark report as unsave with first increase than decrease', ()=> {
+        expect(reportIsSave([1,3,2,4,5])).toBe(false)
+    })
+
+    it('should mark report as unsafe', ()=> {
+        expect(reportIsSave([8,6,4,4,1])).toBe(false)
+    })
+
+    it('should mark report as safe with increase', ()=> {
+        expect(reportIsSave([1,3,6,7,9])).toBe(true)
+    })
+    
+    it('should mark report as unsave with first decrease than increase', ()=> {
+        expect(reportIsSave([8,6,7,2,1])).toBe(false)
     })
 })
